a4b0456c8908fa938def8a3f69f9cb05db5e9686
YaraSamyy <y.samy@egac.rak.ae>
2024-01-24 15:24:01 +0200

    trying redis

--- clients/redisClient.js
+++ clients/redisClient.js
@@ -1,16 +1,16 @@
-const Redis = require('ioredis');
+// const Redis = require('ioredis');
 
-// Replace these values with your ElastiCache Redis endpoint and port
-const redis = new Redis({
-  host: 'complaintcache-wrwwng.serverless.mes1.cache.amazonaws.com',
-  port: 6379,
-});
+// // Replace these values with your ElastiCache Redis endpoint and port
+// const redis = new Redis({
+//   host: 'service-catalog-cache.wrwwng.clustercfg.mes1.cache.amazonaws.com',
+//   port: 6379,
+// });
 
-// Handle connection events
-redis.on('connect', () => {
-  console.log('Connected to Redis');
-});
+// // Handle connection events
+// redis.on('connect', () => {
+//   console.log('Connected to Redis');
+// });
 
-redis.on('error', (err) => {
-  console.error('Redis connection error:', err);
-});
+// redis.on('error', (err) => {
+//   console.error('Redis connection error:', err);
+// });
